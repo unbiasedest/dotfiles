c118dc3152ca2a49bda92329b8ea14b2a562fbe5
Martin Schulze <martinschulze1993@gmail.com>
2015-12-30 16:40:24 +0100

    dotfiles for arch installation

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -5,7 +5,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim tmux.conf bash_aliases"    # list of files/folders to symlink in homedir
+files="bashrc vimrc vim tmux.conf bash_aliases zshrc"    # list of files/folders to symlink in homedir
 
 ##########
 
